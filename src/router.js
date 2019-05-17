3ef189b3e2280d19bf3c61e0182df9f62f1dcf3e
WistreHosshii <sao_heath6147.wistre@icloud.com>
2019-05-17 23:22:55 +0900

    複数ページのvueの作成。login画面の実装

--- src/router.js
+++ src/router.js
@@ -1,24 +1,69 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Axios from './views/Axios.vue'
+import Login from './views/Login.vue'
+import City from './views/City.vue'
+import axios from 'axios'
 
-Vue.use(Router);
+Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        isPublic: true,
+      },
     },
     {
-      path: "/about",
-      name: "about",
+      path: '/axios',
+      name: 'axios',
+      component: Axios,
+      meta: {
+        isPublic: true,
+      },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        isPublic: true,
+      },
+    },
+    {
+      path: '/city/:cityName',
+      name: 'City',
+      component: City,
+    },
+    {
+      path: '/about',
+      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ './views/About.vue'),
+      meta: {
+        isPublic: true,
+      },
+    },
+  ],
+})
+
+router.beforeEach(async (to, from, next) => {
+  try {
+    await axios.get('/api/whoami')
+  } catch (_) {
+    if (to.meta.isPublic) {
+      return next(true)
     }
-  ]
-});
+    return next('/login')
+  }
+  next(true)
+})
+
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" placeholder="userName" v-model="username" />
+    <input type="text" placeholder="password" v-model="password" />
+    <button @click="sendUserName()">send</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    sendUserName() {
+      axios.post('/api/login', {
+        Username: this.username,
+        Password: this.password,
+      })
+    },
+  },
+}
+</script>
